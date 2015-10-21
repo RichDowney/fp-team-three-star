4f0f276fd9f6785324c176fb76740db200ab4a83
Richard Downey <rdowney@bsu.edu>
2015-10-21 01:22:51 -0400

    Add logic to creatJSONObjectTest and make test pass

--- FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/APIValueFileWriterTest.java
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/APIValueFileWriterTest.java
@@ -1,13 +1,19 @@
 package edu.bsu.cs222.twitterbot;
 
 import static org.junit.Assert.*;
+
+import org.json.simple.JSONObject;
 import org.junit.Test;
 
 public class APIValueFileWriterTest {
 	
 	@Test
-	public void createJSONObject() {
-		assertNotNull(null);
+	public void createJSONObjectTest() {
+		String apiKey = "GdiOaLYzitCeCw0yrcqC1YXfP";
+		String apiSecret = "bwtcAcA4kGSkIphO1eqkwANLzahgZ2Z1INmKnfMc8kfgrCmhc6";
+		APIValueFileWriter apiWriter = new APIValueFileWriter(apiKey, apiSecret);
+		JSONObject apiJsonObject = apiWriter.createJSONObject();
+		assertTrue(apiJsonObject.containsValue(apiSecret));
 	}
 
 }
