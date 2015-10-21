603485821514a9a420b44cc4134c5d4ae1daf739
Richard Downey <rdowney@bsu.edu>
2015-10-21 01:30:51 -0400

    Add writeToJsonFile method that writes the apiKey and apiSecret to a txt file

--- FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/APIValueFileWriter.java
+++ FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/APIValueFileWriter.java
@@ -1,29 +1,47 @@
 package edu.bsu.cs222.twitterbot;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 import org.json.simple.JSONObject;
 
 public class APIValueFileWriter {
-	
+
 	private String apiKey;
 	private String apiSecret;
-	
+
 	public APIValueFileWriter(String apiKey, String apiSecret) {
 		this.apiKey = apiKey;
 		this.apiSecret = apiSecret;
 	}
-	
-	public JSONObject createJSONObject(){
+
+	public void tryToWriteToJsonFile() throws IOException {
+		try {
+			writeToJsonFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void writeToJsonFile() throws IOException {
+		JSONObject apiJsonObject = createJSONObject();
+		FileWriter file = new FileWriter("twitter-api-values/api-values.txt");
+		file.write(apiJsonObject.toJSONString());
+		file.flush();
+		file.close();
+	}
+
+	public JSONObject createJSONObject() {
 		JSONObject apiJsonObject = new JSONObject();
 		asMap(apiJsonObject).put("apiKey", this.apiKey);
 		asMap(apiJsonObject).put("apiSecret", this.apiSecret);
 		return apiJsonObject;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	private final static Map<Object,Object> asMap(JSONObject jsonObject) {
-	  return jsonObject;
+	private final static Map<Object, Object> asMap(JSONObject jsonObject) {
+		return jsonObject;
 	}
 	
 }
