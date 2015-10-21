fcfbe1e146d792a21666f6247d85c7485b4150ad
Richard Downey <rdowney@bsu.edu>
2015-10-21 02:28:25 -0400

    Add passing logic to tests

--- FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ReadAPIValuesFromFileTest.java
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ReadAPIValuesFromFileTest.java
@@ -1,19 +1,28 @@
 package edu.bsu.cs222.twitterbot;
 
 import static org.junit.Assert.assertTrue;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class ReadAPIValuesFromFileTest {
 	
+	private ReadAPIValuesFromFile apiValueReader = new ReadAPIValuesFromFile("test-assets/api-values.txt");
+	
+	@Before
+	public void testSetUp() {
+		this.apiValueReader.tryTtoReadFromFile();
+	}
+	
 	@Test
 	public void getAPIKeyTest() {
-		assertTrue(false);
+		String apiKey = this.apiValueReader.getAPIKey();
+		assertTrue(apiKey.equals("GdiOaLYzitCeCw0yrcqC1YXfP"));
 	}
 	
 	@Test
 	public void getAPISecretTest() {
-		assertTrue(false);
+		String apiSecret = this.apiValueReader.getAPISecret();
+		assertTrue(apiSecret.equals("bwtcAcA4kGSkIphO1eqkwANLzahgZ2Z1INmKnfMc8kfgrCmhc6"));
 	}
 
 }
