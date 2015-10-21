82facd722ecf1ccd350aa788d997ed2343cc73eb
Richard Downey <rdowney@bsu.edu>
2015-10-21 02:08:41 -0400

    Add test class for ReadAPIValuesFromFile with getAPISecret and getAPIKey test methods

---
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ReadAPIValuesFromFileTest.java
@@ -0,0 +1,19 @@
+package edu.bsu.cs222.twitterbot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ReadAPIValuesFromFileTest {
+	
+	@Test
+	public void getAPIKeyTest() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void getAPISecretTest() {
+		assertTrue(false);
+	}
+
+}
