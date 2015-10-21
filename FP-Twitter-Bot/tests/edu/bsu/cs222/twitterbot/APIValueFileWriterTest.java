2b0aa480530b0420a98eaca8035cf134b089ebdc
Richard Downey <rdowney@bsu.edu>
2015-10-21 01:13:41 -0400

    Add test for createJSONObject

---
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/APIValueFileWriterTest.java
@@ -0,0 +1,13 @@
+package edu.bsu.cs222.twitterbot;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class APIValueFileWriterTest {
+	
+	@Test
+	public void createJSONObject() {
+		assertNotNull(null);
+	}
+
+}
