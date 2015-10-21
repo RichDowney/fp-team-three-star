37fca725a7088465cb61ff499e6dca45c331d08f
Richard Downey <rdowney@bsu.edu>
2015-10-21 01:58:02 -0400

    Add ReadAPIValuesFromFile class with readFromFile method

---
+++ FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/ReadAPIValuesFromFile.java
@@ -0,0 +1,28 @@
+package edu.bsu.cs222.twitterbot;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ReadAPIValuesFromFile {
+	private JSONObject apiValueJSONObject;
+	private JSONParser parser = new JSONParser();
+	
+	public void tryTtoReadFromFile() {
+		try {
+			readFromFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void readFromFile() throws FileNotFoundException, IOException, ParseException {
+		Object fileObject = parser.parse(new FileReader("twitter-api-values/api-values.txt"));
+		this.apiValueJSONObject = (JSONObject) fileObject;
+	}
+
+}
