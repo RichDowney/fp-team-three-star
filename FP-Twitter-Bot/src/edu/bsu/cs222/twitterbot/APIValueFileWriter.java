ebffd07a0345b7dc2aad9f50a590aa1ad4295a42
Richard Downey <rdowney@bsu.edu>
2015-10-21 00:58:17 -0400

    Add APIValueFileWriter Class with constructor.

---
+++ FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/APIValueFileWriter.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs222.twitterbot;
+
+
+public class APIValueFileWriter {
+	
+	private String apiKey;
+	private String apiSecret;
+	
+	public APIValueFileWriter(String apiKey, String apiSecret) {
+		this.apiKey = apiKey;
+		this.apiSecret = apiSecret;
+	}
+
+}
