5573b702b5fa1d3a2563b8686da93a2ef02ce7f2
Daman Knosp <macbookpro@damans-mbp.dhcp.bsu.edu>
2015-10-22 19:02:37 -0400

    Add passing logic to "WriteAccessTokenValuesToFileTest" class

--- FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/WriteAccessTokenValuesToFileTest.java
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/WriteAccessTokenValuesToFileTest.java
@@ -2,13 +2,17 @@ package edu.bsu.cs222.twitterbot;
 
 import static org.junit.Assert.assertTrue;
 
+import org.json.simple.JSONObject;
 import org.junit.Test;
 
 public class WriteAccessTokenValuesToFileTest {
 	
 	@Test
 	public void createJSONObjectTest() {
-		assertTrue(false);
+		String tokenString = "4000272419-Two6y2CzdG0jpFMeKLVY9MAg4iNVc46SHA1dJBE";
+		String tokenSecret = "l4TSgcMON0OX5AdUagKxLh81hIAJxgt2fgAgtcTVZwarV";
+		WriteAccessTokenValuesToFile tokenWriter = new WriteAccessTokenValuesToFile(tokenString, tokenSecret);
+		JSONObject tokenJsonObject = tokenWriter.createJSONObject();
+		assertTrue(tokenJsonObject.containsValue(tokenSecret));
 	}
-
 }
