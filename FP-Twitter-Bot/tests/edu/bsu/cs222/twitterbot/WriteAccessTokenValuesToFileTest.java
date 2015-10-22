83cba5c4a4e7f1defb284ea5631163b9cd5b9ac4
Daman Knosp <macbookpro@damans-mbp.dhcp.bsu.edu>
2015-10-22 18:36:49 -0400

    Create 'WriteAccessTokenValuesToFileTest' with createJSONObject test method

---
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/WriteAccessTokenValuesToFileTest.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs222.twitterbot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class WriteAccessTokenValuesToFileTest {
+	
+	@Test
+	public void createJSONObjectTest() {
+		assertTrue(false);
+	}
+
+}
