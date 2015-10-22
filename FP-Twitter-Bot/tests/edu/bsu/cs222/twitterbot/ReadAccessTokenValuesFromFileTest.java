81ff164b44140d52961c63af81cbf51616f13a8c
Daman Knosp <macbookpro@damans-mbp.dhcp.bsu.edu>
2015-10-22 18:59:22 -0400

    Add passing logic to tests in "ReadAccessTokenValuesFromFileTest" class

--- FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ReadAccessTokenValuesFromFileTest.java
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ReadAccessTokenValuesFromFileTest.java
@@ -2,18 +2,28 @@ package edu.bsu.cs222.twitterbot;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ReadAccessTokenValuesFromFileTest {
 	
+	private ReadAccessTokenValuesFromFile tokenValueReader = new ReadAccessTokenValuesFromFile("test-assets/token-values.txt");
+	
+	@Before
+	public void testSetUp() {
+		this.tokenValueReader.tryTtoReadFromFile();
+	}
+	
 	@Test
 	public void getTokenStringTest() {
-		assertTrue(false);
+		String tokenString = this.tokenValueReader.getTokenString();
+		assertTrue(tokenString.equals("4000272419-Two6y2CzdG0jpFMeKLVY9MAg4iNVc46SHA1dJBE"));
 	}
 	
 	@Test
 	public void getTokenSecretTest() {
-		assertTrue(false);
+		String tokenSecret = this.tokenValueReader.getTokenSecret();
+		assertTrue(tokenSecret.equals("l4TSgcMON0OX5AdUagKxLh81hIAJxgt2fgAgtcTVZwarV"));
 	}
 
 }
