f4280250f0b8a5b7c34e4d1f9236f06273fe14ee
Daman Knosp <macbookpro@damans-mbp.dhcp.bsu.edu>
2015-10-22 18:40:21 -0400

    Create 'ReadAccessTokenValuesFromFileTest' class with getTokenStringTest() and getTokenSecretTest() methods

---
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ReadAccessTokenValuesFromFileTest.java
@@ -0,0 +1,19 @@
+package edu.bsu.cs222.twitterbot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ReadAccessTokenValuesFromFileTest {
+	
+	@Test
+	public void getTokenStringTest() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void getTokenSecretTest() {
+		assertTrue(false);
+	}
+
+}
