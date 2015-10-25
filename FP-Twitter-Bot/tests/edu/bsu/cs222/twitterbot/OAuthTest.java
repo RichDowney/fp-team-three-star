6e5a0ce9020979400471f512b8fe3468b05db347
Daman Knosp <macbookpro@damans-mbp.dhcp.bsu.edu>
2015-10-25 16:09:21 -0400

    add failing test for 'OAuth' class

---
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/OAuthTest.java
@@ -0,0 +1,16 @@
+package edu.bsu.cs222.twitterbot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OAuthTest {
+	
+	private String apiKey = "GdiOaLYzitCeCw0yrcqC1YXfP";
+	private String apiSecret = "bwtcAcA4kGSkIphO1eqkwANLzahgZ2Z1INmKnfMc8kfgrCmhc6";
+	
+	@Test
+	public void createOAuthServiceTest(){
+		assertTrue(false);
+	}
+}
