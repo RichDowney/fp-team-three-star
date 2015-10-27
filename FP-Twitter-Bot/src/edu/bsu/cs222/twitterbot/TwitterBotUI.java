3994e7d51a182d7f77e2061eaaa77f33ef951dcf
Richard Downey <rdowney@bsu.edu>
2015-10-26 22:48:57 -0400

    Refactor verifyURL name to include authorizaionURL

--- FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/TwitterBotUI.java
+++ FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/TwitterBotUI.java
@@ -22,16 +22,15 @@ public class TwitterBotUI extends Application {
 	}
 	
 	private OAuth oAuth;
-	private String authorizationUrl;
 
 	private GridPane grid = new GridPane();
 	private TextField apiKeyInputField = new TextField();
 	private TextField apiSecretInputField = new TextField();
-	private TextField verifyURLOutputField = new TextField();
+	private TextField authorizationUrlOutputField = new TextField();
 	private TextField tokenVerifierInputField = new TextField();
 	private TextArea tweetTextInputField = new TextArea();
 	private Button postTweetButton = new Button("Post Tweet");
-	private Button getVerifyURLButton = new Button("Get Verify URL");
+	private Button getAuthorizationUrlButton = new Button("Get Authorization URL");
 	private Label apiKeyLabel = new Label("API Key");
 	private Label apiSecretLabel = new Label("API Secret");
 	private Label tokenVerifierLabel = new Label("Token Verifier Code");
@@ -57,11 +56,11 @@ public class TwitterBotUI extends Application {
 	private void addtoGrid() {
 		grid.add(apiKeyInputField, 1, 0);
 		grid.add(apiSecretInputField, 1, 1);
-		grid.add(verifyURLOutputField, 1, 2);
+		grid.add(authorizationUrlOutputField, 1, 2);
 		grid.add(tokenVerifierInputField, 1, 3);
 		grid.add(tweetTextInputField, 1, 4);
 		grid.add(postTweetButton, 1, 5);
-		grid.add(getVerifyURLButton, 0, 2);
+		grid.add(getAuthorizationUrlButton, 0, 2);
 		grid.add(apiKeyLabel, 0, 0);
 		grid.add(apiSecretLabel, 0, 1);
 		grid.add(tokenVerifierLabel, 0, 3);
@@ -71,9 +70,9 @@ public class TwitterBotUI extends Application {
 	private void configureTextFields() {
 		apiKeyInputField.setPromptText("Paste in API Key");
 		apiSecretInputField.setPromptText("Paste in API Secret");
-		verifyURLOutputField.setPromptText("Goto for Verify Code");
-		verifyURLOutputField.setEditable(false);
-		tokenVerifierInputField.setPromptText("Paste in Verify Code");
+		authorizationUrlOutputField.setPromptText("Goto for Authorization Code");
+		authorizationUrlOutputField.setEditable(false);
+		tokenVerifierInputField.setPromptText("Paste in Authorization Code");
 		tweetTextInputField.setPromptText("Tweet limited to 140 Characters");
 		tweetTextInputField.setPrefRowCount(5);
 		tweetTextInputField.setPrefColumnCount(15);
@@ -85,12 +84,12 @@ public class TwitterBotUI extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.sizeToScene();
 		primaryStage.show();
-		setGetVerifyURLButtonAction();
+		setGetAuthorizationUrlButtonAction();
 		setPostTweetButtonAction();
 	}
 	
-	private void setGetVerifyURLButtonAction() {
-		getVerifyURLButton.setOnAction(new EventHandler<ActionEvent>() {
+	private void setGetAuthorizationUrlButtonAction() {
+		getAuthorizationUrlButton.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				generateAuthorizationUrl();
 			}
@@ -113,7 +112,7 @@ public class TwitterBotUI extends Application {
 	
 	private void displayAuthorizationUrl() {
 		String authorizationUrl = oAuth.getAuthorizationUrl();
-		verifyURLOutputField.setText(authorizationUrl);
+		authorizationUrlOutputField.setText(authorizationUrl);
 		
 	}
 
