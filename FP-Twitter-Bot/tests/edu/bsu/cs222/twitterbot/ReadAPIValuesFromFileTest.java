359411afe6126588af6647c4b991a4cbe4f63c92
Richard Downey <rdowney@bsu.edu>
2015-11-16 22:50:17 -0500

    Fix test class to pass with refactored logic

--- FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ReadAPIValuesFromFileTest.java
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ReadAPIValuesFromFileTest.java
@@ -1,27 +1,31 @@
 package edu.bsu.cs222.twitterbot;
 
 import static org.junit.Assert.assertTrue;
+
+import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ReadAPIValuesFromFileTest {
 	
-	private ReadAPIValuesFromFile apiValueReader = new ReadAPIValuesFromFile("test-assets/test-api-values.txt");
+	private ParseFromJSONFile apiValueFileReader;
+	private JSONObject apiFileObject;
 	
 	@Before
 	public void testSetUp() {
-		this.apiValueReader.tryTtoReadFromFile();
+		apiValueFileReader = new ParseFromJSONFile("test-assets/test-api-values.txt");
+		this.apiFileObject = this.apiValueFileReader.tryTtoReadFromFile();
 	}
 	
 	@Test
 	public void getAPIKeyTest() {
-		String apiKey = this.apiValueReader.getAPIKey();
+		String apiKey = this.apiValueFileReader.parseOutObjectValue("apiKey", this.apiFileObject);
 		assertTrue(apiKey.equals("GdiOaLYzitCeCw0yrcqC1YXfP"));
 	}
 	
 	@Test
 	public void getAPISecretTest() {
-		String apiSecret = this.apiValueReader.getAPISecret();
+		String apiSecret = this.apiValueFileReader.parseOutObjectValue("apiSecret", this.apiFileObject);
 		assertTrue(apiSecret.equals("bwtcAcA4kGSkIphO1eqkwANLzahgZ2Z1INmKnfMc8kfgrCmhc6"));
 	}
 
