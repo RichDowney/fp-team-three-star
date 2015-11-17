e0220b6294397b2593260b6941396d121585859c
Richard Downey <rdowney@bsu.edu>
2015-11-16 22:30:51 -0500

    Make methods general for any file

--- FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/ReadAPIValuesFromFile.java
+++ FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/ReadAPIValuesFromFile.java
@@ -9,7 +9,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class ReadAPIValuesFromFile {
-	private JSONObject apiValueJSONObject;
+	private JSONObject fileJSONObject;
 	private JSONParser parser = new JSONParser();
 	private String filePath;
 	
@@ -27,33 +27,17 @@ public class ReadAPIValuesFromFile {
 
 	public void readFromFile() throws FileNotFoundException, IOException, ParseException {
 		Object fileObject = parser.parse(new FileReader(this.filePath));
-		this.apiValueJSONObject = (JSONObject) fileObject;
+		this.fileJSONObject = (JSONObject) fileObject;
 	}
 	
-	public String getAPIKey(){
-		String apiKey = parseOutAPIKey();
-		return apiKey;
+	private String parseOutObjectValue(String key) {
+		String parsedValue = (String) this.fileJSONObject.get(key);
+		return parsedValue;
 	}
 	
-	private String parseOutAPIKey() {
-		String apiKey = (String) this.apiValueJSONObject.get("apiKey");
-		if (apiKey.equalsIgnoreCase("")) {
-			apiKey = "No Saved API Key";
-		}
-		return apiKey;
+	private JSONObject parseOutObject(String key) {
+		JSONObject parsedObject = (JSONObject) this.fileJSONObject.get(key);
+		return parsedObject;
 	}
 	
-	public String getAPISecret(){
-		String apiSecret = parseOutAPISecret();
-		return apiSecret;
-	}
-	
-	private String parseOutAPISecret() {
-		String apiSecret = (String) this.apiValueJSONObject.get("apiSecret");
-		if (apiSecret.equalsIgnoreCase("")) {
-			apiSecret = "No Saved API Secret";
-		}
-		return apiSecret;
-	}
-
 }
