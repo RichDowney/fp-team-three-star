eb5403a789a23dd5ced60d182c65395569a075e7
Richard Downey <rdowney@bsu.edu>
2015-11-17 02:11:25 -0500

    Add tests for token values

--- FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ParseFromJSONFileTest.java
+++ FP-Twitter-Bot/tests/edu/bsu/cs222/twitterbot/ParseFromJSONFileTest.java
@@ -8,25 +8,41 @@ import org.junit.Test;
 
 public class ParseFromJSONFileTest {
 	
-	private ParseFromJSONFile apiValueFileReader;
+	private ParseFromJSONFile apiValueReader;
 	private JSONObject apiFileObject;
+	private ParseFromJSONFile tokenValueReader;
+	private JSONObject tokenFileObject;
 	
 	@Before
 	public void testSetUp() {
-		apiValueFileReader = new ParseFromJSONFile("test-assets/test-api-values.txt");
-		this.apiFileObject = this.apiValueFileReader.tryTtoReadFromFile();
+		apiValueReader = new ParseFromJSONFile("test-assets/test-api-values.txt");
+		this.apiFileObject = this.apiValueReader.tryTtoReadFromFile();
+		tokenValueReader = new ParseFromJSONFile("test-assets/token-values.txt");
+		this.tokenFileObject = this.tokenValueReader.tryTtoReadFromFile();
 	}
 	
 	@Test
 	public void getAPIKeyTest() {
-		String apiKey = this.apiValueFileReader.parseOutObjectValue("apiKey", this.apiFileObject);
+		String apiKey = this.apiValueReader.parseOutObjectValue("apiKey", this.apiFileObject);
 		assertTrue(apiKey.equals("GdiOaLYzitCeCw0yrcqC1YXfP"));
 	}
 	
 	@Test
 	public void getAPISecretTest() {
-		String apiSecret = this.apiValueFileReader.parseOutObjectValue("apiSecret", this.apiFileObject);
+		String apiSecret = this.apiValueReader.parseOutObjectValue("apiSecret", this.apiFileObject);
 		assertTrue(apiSecret.equals("bwtcAcA4kGSkIphO1eqkwANLzahgZ2Z1INmKnfMc8kfgrCmhc6"));
+	}
+	
+	@Test
+	public void getTokenStringTest() {
+		String tokenString = this.tokenValueReader.parseOutObjectValue("tokenString", this.tokenFileObject);
+		assertTrue(tokenString.equals("4000272419-Two6y2CzdG0jpFMeKLVY9MAg4iNVc46SHA1dJBE"));
+	}
+	
+	@Test
+	public void getTokenSecretTest() {
+		String tokenSecret = this.tokenValueReader.parseOutObjectValue("tokenSecret", this.tokenFileObject);
+		assertTrue(tokenSecret.equals("l4TSgcMON0OX5AdUagKxLh81hIAJxgt2fgAgtcTVZwarV"));
 	}
 
 }
