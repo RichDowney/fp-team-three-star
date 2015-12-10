51dfa89a0e4e0628f70a4ebd83618f2e465728d2
Richard Downey <rdowney@bsu.edu>
2015-12-10 18:01:52 -0500

    Refactor method class to new function and reorganize order of methods

--- FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/TwitterBotUI.java
+++ FP-Twitter-Bot/src/edu/bsu/cs222/twitterbot/TwitterBotUI.java
@@ -22,35 +22,37 @@ public class TwitterBotUI extends Application {
 	protected JSONObject usersJSONObject;
 	protected OAuth oAuth;
 	protected AlertFactory alertFactory = new AlertFactory();
+	
 	private LaunchUI launchUI;
-	private AutoTweetUI autoUI;
-	private ManualTweetUI manualUI;
-	private TypeOfTweetUI tweetTypeUI;
 	private ApiUI apiUI;
 	private VerifyUI verifyUI;
+	private TypeOfTweetUI tweetTypeUI;
+	private ManualTweetUI manualUI;
+	private AutoTweetUI autoUI;
 
 	@Override
 	public void start(Stage primaryStage) {
-		launchUI = new LaunchUI(primaryStage, this);
-		launchUI.setUp();
-		autoUI = new AutoTweetUI(primaryStage, this);
-		autoUI.setUp();
-		manualUI = new ManualTweetUI(primaryStage, this);
-		manualUI.setUp();
-		tweetTypeUI = new TypeOfTweetUI(primaryStage, this);
-		tweetTypeUI.setUp();
-		apiUI = new ApiUI(primaryStage, this);
-		apiUI.setUp();
-		verifyUI = new VerifyUI(primaryStage, this);
-		verifyUI.setUp();
+		initUIClasses(primaryStage);
+		setUpAllUIClasses();
 		setStage(primaryStage);
 	}
-
-	protected void setGrid(GridPane grid) {
-		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(15);
-		grid.setVgap(15);
-		grid.setPadding(new Insets(30, 30, 30, 30));
+	
+	private void initUIClasses(Stage primaryStage) {
+		launchUI = new LaunchUI(primaryStage, this);
+		apiUI = new ApiUI(primaryStage, this);
+		verifyUI = new VerifyUI(primaryStage, this);
+		tweetTypeUI = new TypeOfTweetUI(primaryStage, this);
+		manualUI = new ManualTweetUI(primaryStage, this);
+		autoUI = new AutoTweetUI(primaryStage, this);
+	}
+	
+	private void setUpAllUIClasses() {
+		launchUI.setUp();
+		apiUI.setUp();
+		verifyUI.setUp();
+		tweetTypeUI.setUp();
+		manualUI.setUp();
+		autoUI.setUp();
 	}
 	
 	private void setStage(Stage primaryStage) {
@@ -60,9 +62,30 @@ public class TwitterBotUI extends Application {
 		primaryStage.sizeToScene();
 		primaryStage.show();
 	}
+
+	protected void setGrid(GridPane grid) {
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(15);
+		grid.setVgap(15);
+		grid.setPadding(new Insets(30, 30, 30, 30));
+	}
+	
+	protected void switchSceneToStartScene(Stage primaryStage) {
+		primaryStage.setScene(launchUI.getStartScene());
+	}
+	
+	protected void switchSceneToStartSceneAfterSave(Stage primaryStage) {
+		String userName = apiUI.userNameInputField.getText();
+		launchUI.userSelector.getItems().add(userName);
+		primaryStage.setScene(launchUI.getStartScene());
+	}
 	
 	protected void switchSceneToApiScene(Stage primaryStage) {
 		primaryStage.setScene(apiUI.getAPIScene());
+	}
+	
+	protected void switchSceneToVerifyScene(Stage primaryStage) {
+		primaryStage.setScene(verifyUI.getVerifyScene());
 	}
 	
 	protected void switchSceneToTweetTypeScene(Stage primaryStage) {
@@ -85,14 +108,6 @@ public class TwitterBotUI extends Application {
 		oAuth.createAccessTokenFromValues(tokenString, tokenSecret);
 	}
 	
-	protected void switchSceneToTweetScene(Stage primaryStage) {
-		primaryStage.setScene(manualUI.getManualTweetScene());
-}
-	
-	protected void switchSceneToAutomaticTweetScene(Stage primaryStage) {
-			primaryStage.setScene(autoUI.getAutomaticTweetScene());
-	}
-	
 	protected void getApiValuesFromFile() {
 		ParseFromJSONFile apiValueFileReader = new ParseFromJSONFile("twitter-values/api-values.json");
 		JSONObject apiFileObject = apiValueFileReader.tryTtoReadFromFile();
@@ -102,22 +117,16 @@ public class TwitterBotUI extends Application {
 		apiSecret = apiSecretFromFile;
 	}
 	
-	protected void switchSceneToStartScene(Stage primaryStage) {
-		primaryStage.setScene(launchUI.getStartScene());
-	}
-	
-	protected void switchSceneToVerifyScene(Stage primaryStage) {
-		primaryStage.setScene(verifyUI.getVerifyScene());
-	}
-	
 	protected void createOAuthInstance() {
 		oAuth = new OAuth(apiKey, apiSecret);
 	}
 	
-	protected void switchSceneToStartSceneAfterSave(Stage primaryStage) {
-		String userName = apiUI.userNameInputField.getText();
-		launchUI.userSelector.getItems().add(userName);
-		primaryStage.setScene(launchUI.getStartScene());
+	protected void switchSceneToTweetScene(Stage primaryStage) {
+		primaryStage.setScene(manualUI.getManualTweetScene());
+	}
+	
+	protected void switchSceneToAutomaticTweetScene(Stage primaryStage) {
+			primaryStage.setScene(autoUI.getAutomaticTweetScene());
 	}
 	
 	protected String getInputedNewUserName() {
